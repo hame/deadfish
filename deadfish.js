db3140c477f734d6b0b1a240d0161c7e1ae83984
hame <h@hamishosborne.com>
2023-01-21 11:58:12 +1100

    Refactor to a function

--- deadfish.js
+++ deadfish.js
@@ -4,27 +4,31 @@
 // s = square
 // o = output
 
-// Only one memory location (aka register or accumulator)
-let register = 0; // Initise to 0
+// The only memory location (aka register or accumulator)
+let register = 0; // Initialise to 0
 
 // The program
-// Set of instructions that modifies the register
-let program = "dsso";
+let source = "dsso";
+
+// Run program
+interpreter(source);
 
 // The interpreter
-for (let instruction of program) {
-  if (instruction == "i") {
-    register = register + 1;
-  } else if (instruction == "d") {
-    register = register - 1;
-  } else if (instruction == "s") {
-    register = register * register;
-  } else if (instruction == "o") {
-    console.log(register);
-  }
+function interpreter(program) {
+  for (let instruction of program) {
+    if (instruction == "i") {
+      register = register + 1;
+    } else if (instruction == "d") {
+      register = register - 1;
+    } else if (instruction == "s") {
+      register = register * register;
+    } else if (instruction == "o") {
+      console.log(register);
+    }
 
-  // Overflow deadfish style
-  /* Make sure x is not greater then [sic] 256 */
-  if (register == 256) register = 0;
-  if (register == -1) register = 0;
+    // Overflow deadfish style
+    /* Make sure x is not greater then [sic] 256 */
+    if (register == 256) register = 0;
+    if (register == -1) register = 0;
+  }
 }
